c82b62c874bfc19af59697708f2c1642718d5309
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2020-10-24 18:15:40 +0300

    Updates

---
+++ precache-manifest.f1bdd3f512a60bf18541a34b4884b697.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2352082d87d43146d26cda024ea9ce",
+    "url": "/tshakki-ai/index.html"
+  },
+  {
+    "revision": "47a095b92523c5822157",
+    "url": "/tshakki-ai/static/css/main.7861d459.chunk.css"
+  },
+  {
+    "revision": "23843f7ffb332ca2d3d7",
+    "url": "/tshakki-ai/static/js/2.17ee8f8c.chunk.js"
+  },
+  {
+    "revision": "17ad72994a74d92677ed1ba0272bbc77",
+    "url": "/tshakki-ai/static/js/2.17ee8f8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47a095b92523c5822157",
+    "url": "/tshakki-ai/static/js/main.d3bc4d52.chunk.js"
+  },
+  {
+    "revision": "b3e1c0c63340e2fc8c44",
+    "url": "/tshakki-ai/static/js/runtime-main.f7144151.js"
+  }
+]);
